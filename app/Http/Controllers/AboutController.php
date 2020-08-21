8b97202fdab36e816d2ff0ead40cee4205811995
Sumaiya <sumaiyahadayet7@gmail.com>
2020-08-21 20:14:27 +0600

    changes

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -14,7 +14,7 @@ class AboutController extends Controller
      *
      * @return void
      */
-    public function __construct()
+    public function __construct() 
     {
         $this->middleware('role:superadministrator');
     }
